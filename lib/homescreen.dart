2a0d448c36fa2e418e48c42f22de4bfc50fc5241
1AnkitSingh02 <ankitsingh4301@gmail.com>
2020-06-27 15:41:43 +0530

    patch 1

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -2,6 +2,8 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:snatched/profile.dart';
+import "Widget/BottomNavBarWidget.dart";
+import 'Widget/SearchWidget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -10,57 +12,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage>{
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    profile(),
-    Restro(),
-    History(),
-
-
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-//        title: const Text('BottomNavigationBar Sample'),
+//      appBar: AppBar(
+//        backgroundColor: Color(0xFFFAFAFA),
+//        elevation: 0,
+//        brightness: Brightness.light,
+//        actions: <Widget>[
+//
+//        ],
+//      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SearchWidget(),
+            //RestroWidget(),
+          ],
+        ),
       ),
-      body:
-      Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant_menu
-            ),
-
-            title: Text('Business'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restore),
-            title: Text('School'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromRGBO(255, 2, 102, 0),
-        onTap: _onItemTapped,
-      ),
+      bottomNavigationBar: BottomNavBarWidget(),
     );
   }
 }
+
+
 
 
 
