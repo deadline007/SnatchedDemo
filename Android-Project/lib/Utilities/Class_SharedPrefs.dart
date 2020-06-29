905fb39d3ed67c652572a3e3f538725c2c1a956e
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-06-29 23:09:00 +0530

    lil changes

--- Android-Project/lib/Utilities/Class_SharedPrefs.dart
+++ Android-Project/lib/Utilities/Class_SharedPrefs.dart
@@ -2,15 +2,26 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 //Manage local shared prefs
 class ClassSharedPref {
-  static String key = "isSeen"; //is this the first time app is being opened
+  static String keyBool = "isLoggedIn"; //is the user logged in ?
+  var prefFile;
+  ClassSharedPref() {
+    getFile();
+  }
+
+  Future<void> getFile() async {
+    prefFile = await SharedPreferences.getInstance();
+  }
+
   Future<bool> getState() async {
-    final prefFile = await SharedPreferences.getInstance();
-    final getBool = prefFile.getBool(key);
+    final getBool = prefFile.getBool(keyBool);
     if (getBool == null) {
-      prefFile.setBool(key, true);
       return false;
     } else {
       return true;
     }
+  }
+
+  Future<void> setState() async {
+    prefFile.setBool(keyBool, true);
   }
 }
