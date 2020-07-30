a21766b252c258c8e8ab82c12316974d398800b4
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-07-30 12:03:18 +0530

    Update Route_Profile.dart

--- lib/Routes/Route_Profile.dart
+++ lib/Routes/Route_Profile.dart
@@ -10,24 +10,59 @@ class RouteProfile {
   final double heightMax = ClassScreenConf.vArea;
   final String fontDef = ClassAssetHolder.proximaLight;
   final Color colorDef = ClassAssetHolder.mainColor;
+  Color topColor = Colors.white;
+
+  RouteProfile() {}
 
   Widget buildProfile(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        overflow: Overflow.visible,
-        alignment: Alignment.topLeft,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            child: Container(
-              height: heightMin * 30,
-              width: widthMax,
-              alignment: Alignment.topLeft,
-              color: Colors.red,
+      body: Container(
+        width: widthMax,
+        height: heightMax,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Colors.pink[100],
+              Colors.deepOrange[100],
+            ],
+            
+          ),
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          overflow: Overflow.visible,
+          alignment: Alignment.topLeft,
+          children: <Widget>[
+            Positioned(
+              top: -heightMin * 20,
+              left: -widthMin * 50,
+              child: Container(
+                height: heightMin * 70,
+                width: widthMax * 2,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+              ),
             ),
-          )
-        ],
+            Positioned(
+              top: heightMin * 35,
+              child: Container(
+                width: widthMax,
+                height: heightMin * 18,
+                child: Center(
+                  child: Container(
+                    width: widthMin * 36,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
