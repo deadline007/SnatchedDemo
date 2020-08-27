798fb40b1a14c1b2c8ae404ed356f5aa3db9232b
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-08-27 23:57:45 +0530

    vendormenu wip

---
+++ lib/Routes/Route_VendorMenu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:snatched/Data/Class_VendorData.dart';
+
+class RouteVendorMenu extends StatefulWidget {
+  final ClassVendorData vendorData;
+  RouteVendorMenu({Key key, this.vendorData}) : super(key: key);
+  @override
+  _RouteVendorMenuState createState() => _RouteVendorMenuState();
+}
+
+class _RouteVendorMenuState extends State<RouteVendorMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
